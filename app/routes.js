e855d43b5022de24676be59bcd872a14322fcf22
Megan Rodger <megan.rodger@phe.gov.uk>
2018-12-17 17:37:43 +0000

    first start to prototype journey

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,37 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.get('/v1/historic-start', function (req, res){
+var what = req.query.what
+
+if (what === 'search') {
+res.redirect('search/search-start')
+}
+
+else if (what === 'team') {
+res.redirect('team/team-start')
+}
+
+else {
+res.render('v1/historic/historic-start')
+}
+
+});
+
+router.get('/v1/team/how-doing', function (req, res){
+var view_by = req.query.view_by
+
+if (view_by === 'by_what') {
+res.redirect('what-should')
+}
+
+else if (view_by === 'by_need') {
+res.redirect('what-need')
+}
+
+else {
+res.render('v1/team/how-doing')
+}
+
+});
