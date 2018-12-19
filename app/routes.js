b529593d804ac8c12601f71af4977ac364f51bdc
Megan Rodger <megan.rodger@phe.gov.uk>
2018-12-19 17:27:05 +0000

    padding out the best practice route

--- app/routes.js
+++ app/routes.js
@@ -11,16 +11,14 @@ var what = req.query.what
 if (what === 'search') {
 res.redirect('search/search-start')
 }
-
 else if (what === 'team') {
 res.redirect('team/team-start')
 }
-
 else {
 res.render('v1/historic/historic-start')
 }
-
 });
+
 
 router.get('/v1/team/how-doing', function (req, res){
 var view_by = req.query.view_by
@@ -28,13 +26,25 @@ var view_by = req.query.view_by
 if (view_by === 'by_what') {
 res.redirect('what-should')
 }
-
 else if (view_by === 'by_need') {
 res.redirect('what-need')
 }
-
 else {
 res.render('v1/team/how-doing')
 }
+});
 
+
+router.get('/v1/team/search-all', function (req, res){
+var which_practice = req.query.which_practice
+
+if (which_practice === 'tagged_bp') {
+res.redirect('tagged-best-practice')
+}
+else if (which_practice === 'uploaded_ce') {
+res.redirect('uploaded-engagement')
+}
+else {
+res.render('v1/team/search-all')
+}
 });
